44a35910c03bef43cb1315ff08a9f0d2a622358c
Jordan Lapp <negascout@gmail.com>
2024-03-23 12:35:09 -0700

    fixing google

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
 FROM node:alpine
 
+# Create app directory
 RUN mkdir -p /usr/src/bianca-app && chown -R node:node /usr/src/bianca-app
-
 WORKDIR /usr/src/bianca-app
 
+# Install app dependencies
 COPY package.json yarn.lock ./
-
 USER node
-
 RUN yarn install --pure-lockfile
 
+# Bundle app source
 COPY --chown=node:node . .
 
+# Copy .env file
+COPY --chown=node:node .env ./
+
+# Expose the port the app runs on
 EXPOSE 3000
+
+# Command to run your application
+CMD ["node", "app.js"]
---
+++ install-mongo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Update the instance
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Install AWS CLI if it's not installed
+if [ ! -x "$(command -v aws)" ]; then
+    echo "Installing AWS CLI..."
+    sudo apt-get -y install awscli
+else
+    echo "AWS CLI already installed."
+fi
+
+# Install Docker if it's not installed
+if [ ! -x "$(command -v docker)" ]; then
+    echo "Installing Docker..."
+    sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get -y install docker-ce
+
+    # Add the current user to the Docker group to avoid needing sudo
+    sudo usermod -aG docker $USER
+    newgrp docker
+else
+    echo "Docker already installed."
+fi
+
+# Install Docker Compose if it's not installed
+if [ ! -x "$(command -v docker-compose)" ]; then
+    echo "Installing Docker Compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+else
+    echo "Docker Compose already installed."
+fi
+
+# Login to Amazon ECR
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin mongo:4.2.1-bionic
+
+# Pull the Docker container
+docker pull mongo:4.2.1-bionic
+
+# Run the Docker container
+# Adjust the following command according to your container's needs (ports, environment variables, etc.)
+docker run -d --name mongodb -p 27017:27017 mongo:4.2.1-bionic
