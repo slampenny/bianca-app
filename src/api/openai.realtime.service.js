e2559c8dca5e558d80cc0e02fa8b129d206c8e9f
Jordan Lapp <negascout@gmail.com>
2025-06-03 06:53:00 -0700

    still trying to debug

--- src/api/openai.realtime.service.js
+++ src/api/openai.realtime.service.js
@@ -1500,7 +1500,7 @@ initializeContinuousDebugFiles(callId) {
  * Only uploads 2 files: one combined file from Asterisk and one from OpenAI
  */
 async uploadDebugAudioToS3(callId) {
-    const S3Service = require('./s3.service');
+    const S3Service = require('../services/s3.service');
     
     try {
         const callAudioDir = path.join(DEBUG_AUDIO_LOCAL_DIR, callId);
