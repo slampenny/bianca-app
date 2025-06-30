3ebbb14777d2f62f87b0328709802485480fdc68
Jordan Lapp <negascout@gmail.com>
2025-06-29 17:49:35 -0700

    triggering deploy

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -216,7 +216,7 @@ variable "github_repo" {
 variable "github_branch" {
   description = "GitHub branch for CodePipeline."
   type        = string
-  default     = "main"
+  default     = "audio/fix-networking"
 }
 
 variable "github_app_connection_arn" {
